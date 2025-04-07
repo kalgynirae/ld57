9c0dfa56460b8c3bb9184cf968b9a9b93011f3b3
Sam Fu <shunhua.fu@gmail.com>
2025-04-07 12:31:05 -0700

    transition to race by setting visibility

--- game.gd
+++ game.gd
@@ -26,26 +26,42 @@ func _process(_delta):
 
 func advance_mode():
 	mode = (mode + 1) % 5
+	
+	$RaceScreen.visible = false
+
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUAD)
 
 	if mode == 0:
 		$MusicTitleIntro.play()
 		position = Vector2(0, 0)
-	else:
+	elif mode < 4:
 		$MusicTitleLoop.stop()
 		$MusicTitleIntro.stop()
-		var tween = get_tree().create_tween()
-		tween.set_ease(Tween.EASE_OUT)
-		tween.set_trans(Tween.TRANS_QUAD)
+		var target_y = 0
+		match mode:
+			1:
+				target_y = -$CharacterScreen.position.y
+			2:
+				target_y = -$OpponentScreen.position.y
+			3:
+				target_y = -$TrackScreen.position.y
+		tween.tween_property(self, "position", Vector2(0, target_y), 0.75)
+	elif mode == 4:
 		# Fade to black for entering race scene
-		if mode == 4:
-			tween.tween_property(self, "modulate", Color(0, 0, 0, 1), 0.6)
-		tween.tween_property(self, "position", Vector2(0, -720 * mode), 0.6)
-		if mode == 4:
-			tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.6)
-		tween.finished.connect(_on_transition_finished)
+		tween.tween_property(self, "modulate", Color(0, 0, 0, 1), 0.6)
+		tween.tween_callback(_on_faded_to_black)
+		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.6)
+	
+	tween.finished.connect(_on_transition_finished)
 
 	if mode == 4:
 		$RaceScreen.init(completed_first_race, "millie")
+
+func _on_faded_to_black() -> void:
+	if mode == 4:
+		$RaceScreen.visible = true
 
 func _on_transition_finished() -> void:
 	if mode == 4:
